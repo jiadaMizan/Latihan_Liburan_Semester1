74aa7bc13a6f2a4396e51e536c1b84ddf8f8e3ed
jiadaMizan <jiada1308@gmail.com>
2026-01-06 16:44:51 +0700

    latihan CM array 1d

---
+++ LatihanCaseMethod2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+    public class LatihanCaseMethod2 {
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+
+            int jmlMhs = 0, nilaiTotal=0, tertinggi = 0, terendah = 100;
+            System.out.print("Masukkan banyaknya nilai mahasiswa yang akan diinput : ");
+            jmlMhs = sc.nextInt();
+
+            int [] nilaiMhs = new int [jmlMhs];
+  
+            for (int x = 0; x < jmlMhs; x++) {
+                System.out.print("Masukkan nilai mahasiswa ke-" + (x+1) + " : ");
+                nilaiMhs[x] = sc.nextInt();
+                nilaiTotal += nilaiMhs[x];
+            }
+
+            for (int x = 0; x < jmlMhs; x++) {
+                if (nilaiMhs[x] < terendah) {
+                    terendah = nilaiMhs[x];
+                } else if (nilaiMhs[x] > tertinggi) {
+                    tertinggi = nilaiMhs[x];
+                }
+            }
+
+            double rataNilai = nilaiTotal/jmlMhs;
+
+            System.out.println("Nilai total seluruh mahasiswa adalah : " +nilaiTotal);
+            System.out.println("Nilai rata-rata mahasiswa adalah : " +rataNilai);
+            System.out.println("Nilai mahasiswa tertinggi adalah : " +tertinggi);
+            System.out.println("Nilai mahasiswa terendah adalah : " +terendah);
+            sc.close();
+        }
+    }
