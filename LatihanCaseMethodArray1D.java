ef3bf14e1728e009c42e8ad71ac4b2b488bdcbc9
jiadaMizan <jiada1308@gmail.com>
2026-01-12 13:30:11 +0700

    penulisan ulang kode

--- LatihanCaseMethodArray1D.java
+++ LatihanCaseMethodArray1D.java
@@ -1,39 +1,11 @@
 import java.util.Scanner;
 public class LatihanCaseMethodArray1D {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int hargaTotal = 0;
+         System.out.print("Masukkan banyaknya nilai yang akan diinput : ");
+         int jmlNilai = sc.nextInt();
 
-        System.out.print("Masukkan jumlah pesanan : ");
-        int jmlPesanan = sc.nextInt();
-        System.out.println();
-        sc.nextLine();
-
-        String [] namaPesanan = new String [jmlPesanan];
-        int [] hargaPesanan = new int [jmlPesanan];
-
-        for (int x = 0 ; x < jmlPesanan ; x++) {
-            System.out.println("Pesanan ke-" + (x+1));
-            System.out.print("Masukkan nama makanan ke-" + (x+1) + " : ");
-            String nama = sc.nextLine();
-            System.out.print("Masukkan harga " + (nama) + " : ");
-            int harga = sc.nextInt();
-            System.out.println();
-            sc.nextLine();
-            namaPesanan[x] = nama;
-            hargaPesanan[x] = harga;
-            hargaTotal += harga;
-        }
-
-        System.out.println("=====DAFTAR PESANAN=====");
-        for (int x = 0 ; x < jmlPesanan ; x++) {
-            System.out.println((x+1) + ". " + (namaPesanan[x]) + " - Rp " + (hargaPesanan[x]));
-        }
-
-        System.out.println("====================================");
-        System.out.println("Total biaya : Rp " + (hargaTotal));
-
-        sc.close();
+         int [] dataNilai = new int [jmlNilai];
     }
 }
