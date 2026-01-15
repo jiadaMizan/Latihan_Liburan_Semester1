6c7cd8b6f5e8ecff26a5e937ffa501acc68ee5f4
jiadaMizan <jiada1308@gmail.com>
2026-01-15 10:47:45 +0700

    checkpoint

--- LatihanCaseMethodWhileKASIR.java
+++ LatihanCaseMethodWhileKASIR.java
@@ -4,7 +4,7 @@ public class LatihanCaseMethodWhileKASIR {
         Scanner sc = new Scanner (System.in);
 
         boolean konfirmasi=true;
-        int totPilih = 0, totJmlBeli = 0, jmlPerulangan = 0;
+        int totPilih = 0, totJmlBeli = 0, jmlPerulangan = 0, harga, totHarga = 0, totKeseluruhan = 0;
 
         System.out.println("=== TOKO MADURA SUNDA PAK ASEP ===");
         System.out.println();
@@ -17,19 +17,44 @@ public class LatihanCaseMethodWhileKASIR {
             System.out.println("4. Penggaris  : 4000");
             System.out.print("Pilih kode nomor barang (1-4) : ");
             int pilih = sc.nextInt();
+            if (pilih > 4) {
+                System.out.println("Kode nomor barang tidak valid, masukkan (1-4)!");
+                System.out.println();
+                continue;
+            }
             totPilih += pilih;
             System.out.print("Jumlah beli : ");
             int jmlBeli = sc.nextInt();
             totJmlBeli += jmlBeli;
             sc.nextLine();
+            System.out.println();
+
+            if (pilih == 1) {
+                harga = 5000;
+                totHarga = harga * jmlBeli;
+            } else if (pilih == 2) {
+                harga = 3000;
+                totHarga = harga * jmlBeli;
+            } else if (pilih == 3) {
+                harga = 2000;
+                totHarga = harga * jmlBeli;
+            } else if (pilih == 4) {
+                harga = 4000;
+                totHarga = harga * jmlBeli;
+            }
+            totKeseluruhan += totHarga;
 
             System.out.print("Apakah anda ingin melanjutkan pembelian? (y/n)");
             String lanjut = sc.nextLine();
-            
+            System.out.println();
+
             if (lanjut.equalsIgnoreCase("n") ) {
                 konfirmasi = false;
             }
-            jmlPerulangan++;
         }
+
+        System.out.println("--- Total Belanjaan Anda---");
+        System.out.println("Total harga yang harus dibayar : Rp." + (totKeseluruhan));
+        System.out.println("");
     }
 }
